ad28bb0cfa325e3465ab5291be3a369639578eea
ram <ramireddy251@gmail.com>
2026-02-04 12:29:54 +0530

    added rabbitmq file

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -15,8 +15,14 @@ systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
 VALIDATE $? "Enable and start rabbitmq server"
 
-rabbitmqctl add_user roboshop roboshop123 &>>$LOGS_FILE
-VALIDATE $? "adding roboshot user and password"
+rabbitmqctl list_users | grep -w roboshop &>>$LOGS_FILE
+
+if [ $? -ne 0 ]; then
+    rabbitmqctl add_user roboshop roboshop123 &>>$LOGS_FILE
+    VALIDATE $? "adding roboshop user and password"
+else
+    echo -e "roboshop user already exists ... $Y SKIPPING $N" | tee -a $LOGS_FILE
+fi
 
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOGS_FILE
 VALIDATE $? "setting permissions to roboshop user"
